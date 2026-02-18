21adac0a7deb508e71f8fc96597268f6b99585ba
IntegralTeam <repo@sfxdx.com>
2026-02-18 10:48:19 +0700

    Update Limo program ID and wallet path in Anchor.toml, and add address metadata to Limo IDL

--- programs/limo/src/lib.rs
+++ programs/limo/src/lib.rs
@@ -21,7 +21,7 @@ use utils::{
 use crate::handlers::*;
 pub use crate::state::*;
 
-declare_id!("82qR3ftARSeXPBg9wnVrsLsAepfvimBUCPdNuiV9unhV");
+declare_id!("BQvCNSSpC4Csn5Ldr7GmLvrXnjLjLJRsDhYNMpkgJskS");
 
 #[cfg(not(feature = "no-entrypoint"))]
 solana_security_txt::security_txt! {
